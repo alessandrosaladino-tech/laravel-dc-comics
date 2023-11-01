399998fbafeaf6f66322085dc2e5d4132ee60c65
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-01 14:44:07 +0100

    Add
    Migration, PageController and model

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Comic;
+
+class PageController extends Controller
+{
+    //
+}
