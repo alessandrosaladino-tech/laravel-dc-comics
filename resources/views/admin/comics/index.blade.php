1b01d61cf2c719d6c6e4f12c3781beb7279ba653
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-03 16:15:31 +0100

    Add
    Delete CRUD Operation

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -31,12 +31,45 @@
                             </td>
                             <td>
                                 <a class="btn btn-primary" href="{{ route('comics.show', $comic->id) }}">More</a>
-                                <a class="btn btn-success" href="{{route('comics.edit', $comic->id)}}">Edit</a>
-                                <a class="btn btn-danger" href="#">Delete</a>
+                                <a class="btn btn-success" href="{{ route('comics.edit', $comic->id) }}">Edit</a>
+
+                                <!-- Modal for deleting comics with danger message -->
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $comic->id }}">
+                                    Delete
+                                </button>
+
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitleId-{{ $comic->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitleId-{{ $comic->id }}">Deleting Comic #{{ $comic->title }}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">Are you sure you want to delete the comic?</div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-primary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Yes, i want to delete</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     @empty
-                        <h6>There are any comic in here!</h6>
+                        <h6>There are no comics for the moment, Sorry</h6>
                     @endforelse
                 </tbody>
             </table>
